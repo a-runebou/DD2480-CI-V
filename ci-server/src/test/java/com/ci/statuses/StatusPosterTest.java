64053d03b32bf714e68216ab5d96ec17135eeb6f
kahoujo1 <kahounjosef3@gmail.com>
2026-02-03 12:44:51 +0100

    feat: implement commit status notifications (#8)

--- ci-server/src/test/java/com/ci/statuses/StatusPosterTest.java
+++ ci-server/src/test/java/com/ci/statuses/StatusPosterTest.java
@@ -1,6 +1,9 @@
 package com.ci.statuses;
 
+import javax.naming.InsufficientResourcesException;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
 public class StatusPosterTest {
@@ -13,6 +16,61 @@ public class StatusPosterTest {
     void testConstructorDoesNotThrow() {
         assertDoesNotThrow(() -> {
             StatusPoster sp = new StatusPoster();
+        });
+    }
+
+
+    /**
+     * Contract:
+     * The GitHub API accepts only error, failure, pending, or success values for the state.
+     * 
+     * Expected behavior:
+     * Given any other value for the state, the function shall throw IllegalArgumentException.
+     * @throws InsufficientResourcesException 
+     */
+    @Test
+    void invalidStateThrowsIllegalArgumentException() {
+        StatusPoster sp = new StatusPoster();
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> sp.postStatus("xxx", "testState", "testUrl", "test description")
+        );
+    }
+
+
+    /**
+     * Contract:
+     * The postStatus function shall post a new commit status to the github API, and receive a response
+     * with status code 201 if everything went correctly, or different if an error occured. If status
+     * code is different from 201, the function shall throw RuntimeException.
+     * 
+     * Expected behavior:
+     * Given wrong SHA of a non-existing commit, the function shall throw RuntimeException.
+     */
+    @Test
+    void wrongShaResultsInRuntimeException() {
+        StatusPoster sp = new StatusPoster();
+        assertThrows(RuntimeException.class, () -> {
+            sp.postStatus(null, "success",
+             "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1", "test description");
+        });
+    }
+
+    /**
+     * Contract:
+     * The postStatus function shall post a new commit status to the github API, and receive a response
+     * with status code 201 if everything went correctly, or different if an error occured. If status
+     * code is different from 201, the function shall throw RuntimeException.
+     * 
+     * Expected behavior:
+     * Given a valid SHA of an existing commit, the function shall complete without throwing exceptions.
+     */
+    @Test
+    void validShaDoesNotThrow() {
+        StatusPoster sp = new StatusPoster();
+        assertDoesNotThrow(()-> {
+            sp.postStatus("7f37d2da1b409a2ce60b8a068d081745372f7f39", "error", // test commit on the issue/8-test-branch
+            "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1", "test description"); 
         });
     }
 }
