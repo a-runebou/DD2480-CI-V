b00ca7732c9fffdda749b85a28b9514e8b2fed4f
Alexander Runebou <alexanderrunebou02@gmail.com>
2026-02-04 17:11:34 +0100

    feat: run server by default in App (#4)

--- ci-server/src/main/java/com/ci/App.java
+++ ci-server/src/main/java/com/ci/App.java
@@ -1,7 +1,15 @@
 package com.ci;
 
+import java.io.IOException;
+
 public class App {
     public static void main( String[] args ) {
+        Server server = new Server();
+        try {
+            server.start();
+        } catch (IOException ex) {
+            System.getLogger(App.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
         System.out.println( "Hello World!" );
     }
 }
